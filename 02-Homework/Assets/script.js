9b280316174876d2806aa3363a5fd43efb31d06f
NeoGabe <gabrielg28@hotmail.com>
2020-11-21 22:44:20 -0600

    worked on bugs

--- 02-Homework/Assets/script.js
+++ 02-Homework/Assets/script.js
@@ -3,9 +3,20 @@ var inputValue = document.querySelector(".inputValue");
 var name = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
+var wind = document.querySelector(".wind");
 
 button.addEventListener("click", function() {
-fetch("http://api.openweathermap.org/data/2.5/forecast?q=524901&appid=7bf549fe012f11d0d2b454d903e7e46a")
+fetch("http://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=7bf549fe012f11d0d2b454d903e7e46a")
         .then(response => response.json())
-        .then(data => console.log(data))
-});
+        .then(data => {
+            var nameValue = data["name"];
+            var tempValue = data["main"]["feels_like", "humidity", "pressure","temp"];
+            var descValue = data["weather"][0]["description"];
+            var windValue = data["wind"]["speed"];
+            console.log(data);
+            name.innerHTML = nameValue;
+            temp.innerHTML = tempValue;
+            desc.innerHTML = descValue;
+            wind.innerHTML = windValue;
+        })
+    });
