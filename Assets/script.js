d9ef05719e5d06ceba7c5fb67deb43892c64ba2a
NeoGabe <gabrielg28@hotmail.com>
2020-12-03 14:22:46 -0600

    changed class name for city in js for functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 var button = document.querySelector(".button");
 var inputValue = document.querySelector(".inputValue");
-var name = document.querySelector(".name");
+var city = document.querySelector(".city");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
 var wind = document.querySelector(".wind");
@@ -9,12 +9,12 @@ button.addEventListener("click", function() {
 fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=7bf549fe012f11d0d2b454d903e7e46a")
         .then(response => response.json())
         .then(data => {
-            var nameValue = data["name"];
+            var cityValue = data["name"];
             var tempValue = data["main"]["temp", "feels_like","temp_min", "temp_max"];
             var descValue = data["weather"][0]["description"];
             var windValue = data["wind"]["speed"];
-            
-            name.innerHTML = nameValue;
+            console.log(city)
+            city.innerHTML = cityValue;
             desc.innerHTML = "Clouds: " +descValue;
             temp.innerHTML = "Tempature: " +tempValue;
             wind.innerHTML = "Winds: " +windValue;
